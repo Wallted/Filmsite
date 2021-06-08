143f57648bda250108eef586d7a1fe8e321248d4
Grzegorz Choiński <grzegorzchoinski97@gmail.com>
2021-06-08 17:39:41 +0200

    Fix admin verbose names
    Signed-off-by: Grzegorz Choiński <grzegorzchoinski97@gmail.com>

--- filmsite/app/models.py
+++ filmsite/app/models.py
@@ -11,61 +11,73 @@ from PIL import Image, ImageOps
 
 
 class Movie(models.Model):
-    title = models.CharField(max_length=60)
-    description = models.CharField(max_length=250)
-    launch = models.DateField()
-    lenght = models.IntegerField()
-    genre = models.CharField(max_length=60)
-    director = models.CharField(max_length=60)
-    poster = models.ImageField(upload_to='posters/')
-    keywords = models.CharField(max_length=250, null=True)
+    title = models.CharField(max_length=60, verbose_name='Tytuł')
+    description = models.CharField(max_length=250, verbose_name='Opis')
+    launch = models.DateField(verbose_name='Premiera')
+    lenght = models.IntegerField(verbose_name='Długość')
+    genre = models.CharField(max_length=60, verbose_name='Gatunek')
+    director = models.CharField(max_length=60, verbose_name='Reżyser')
+    poster = models.ImageField(upload_to='posters/', verbose_name='Plakat')
+    keywords = models.CharField(max_length=250, null=True, verbose_name='Słowa kluczowe')
 
     def __str__(self):
         return f"{self.title}, {self.director} ({str(self.launch).split('-')[0]})"
 
+    class Meta:
+        verbose_name = 'Film'
+        verbose_name_plural = 'Filmy'
+
 
 class Post(models.Model):
-    title = models.CharField(max_length=60, null=True, blank=True)
-    contents = models.CharField(max_length=1000)
-    date = models.DateTimeField(auto_now_add=True, null=True)
+    title = models.CharField(max_length=60, null=True, blank=True, verbose_name='Tytuł')
+    contents = models.CharField(max_length=1000, verbose_name='Treść')
+    date = models.DateTimeField(auto_now_add=True, null=True, verbose_name='Data wpisu')
 
     movie = ForeignKey('Movie', related_name='thread',
-                       on_delete=CASCADE, null=True, blank=True)
+                       on_delete=CASCADE, null=True, blank=True, verbose_name='Film')
     parent = ForeignKey('Post', related_name='response',
-                        on_delete=CASCADE, null=True, blank=True)
+                        on_delete=CASCADE, null=True, blank=True, verbose_name='Wątek główny')
     user = ForeignKey(User, related_name='post',
-                      on_delete=CASCADE, null=True, blank=True)
+                      on_delete=CASCADE, null=True, blank=True, verbose_name='Użytkownik')
 
     def __str__(self):
         if self.date == None:
             return '---'
         return f"{self.title}, {self.date}"
 
+    class Meta:
+        verbose_name = 'Wpis'
+        verbose_name_plural = 'Wpisy'
+
 
 class Opinion(models.Model):
     RATES = Choices(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
 
-    rate = models.IntegerField(choices=RATES, null=True)
-    is_favourite = models.BooleanField(null=True)
-    want_to_watch = models.BooleanField(null=True , blank=True)
-    contents = models.CharField(max_length=250, null=True, blank=True, default='Brak opinii.')
-    date = models.DateTimeField(auto_now_add=True)
+    rate = models.IntegerField(choices=RATES, null=True, verbose_name='Ocena')
+    is_favourite = models.BooleanField(null=True, verbose_name='Ulubiony')
+    want_to_watch = models.BooleanField(null=True , blank=True, verbose_name='Chce zobaczyć')
+    contents = models.CharField(max_length=250, null=True, blank=True, default='Brak opinii.', verbose_name='Treść')
+    date = models.DateTimeField(auto_now_add=True, verbose_name='Data')
 
     movie = ForeignKey('Movie', related_name='opinion',
-                       on_delete=CASCADE, null=True, blank=True)
+                       on_delete=CASCADE, null=True, blank=True, verbose_name='Film')
     user = ForeignKey(User, related_name='opinion',
-                      on_delete=CASCADE, null=True, blank=True)
+                      on_delete=CASCADE, null=True, blank=True, verbose_name='Użytkownik')
 
     def __str__(self):
         return f"opinia {self.user} o {self.movie}"
 
+    class Meta:
+        verbose_name = 'Opinia'
+        verbose_name_plural = 'Opinie'
+
 
 class Photo(models.Model):
-    img = models.ImageField(upload_to='images/')
-    description = models.CharField(max_length=60, null=True)
+    img = models.ImageField(upload_to='images/', verbose_name='Zdjęcie')
+    description = models.CharField(max_length=60, null=True, verbose_name='Opis')
 
     movie = ForeignKey('Movie', related_name='photo',
-                       on_delete=CASCADE, null=True, blank=True)
+                       on_delete=CASCADE, null=True, blank=True, verbose_name='Film')
 
     def save(self):
         super(Photo, self).save()
@@ -82,21 +94,28 @@ class Photo(models.Model):
     def __str__(self):
         return f"{self.movie.title}, {self.description}"
 
+    class Meta:
+        verbose_name = 'Zdjęcie'
+        verbose_name_plural = 'Zdjęcia'
 
 class PhotoActor(models.Model):
     photo = ForeignKey('Photo', related_name='actor',
-                       on_delete=CASCADE, null=True, blank=True)
+                       on_delete=CASCADE, null=True, blank=True, verbose_name='Zdjęcie')
     actor = ForeignKey('Actor', related_name='photo',
-                       on_delete=CASCADE, null=True, blank=True)
+                       on_delete=CASCADE, null=True, blank=True, verbose_name='Aktor')
 
     def __str__(self):
         return f"{self.actor}, {self.photo}"
 
+    class Meta:
+        verbose_name = 'Zdjęcie z Aktorem'
+        verbose_name_plural = 'Zdjęcia z Aktorami'
+
 class Actor(models.Model):
-    name = models.CharField(max_length=60)
-    portrait = models.ImageField(upload_to='portraits/')
-    birth = models.DateField(null=True)
-    bio = models.CharField(max_length=250, default='Brak notki biograficznej.')
+    name = models.CharField(max_length=60, verbose_name='Nazwisko')
+    portrait = models.ImageField(upload_to='portraits/', verbose_name='Portret')
+    birth = models.DateField(null=True, verbose_name='Data urodzenia')
+    bio = models.CharField(max_length=250, default='Brak notki biograficznej.', verbose_name='Bio')
 
     def __str__(self):
         return f"{self.name}"
@@ -113,25 +132,32 @@ class Actor(models.Model):
 
         super(Actor, self).save()
 
+    class Meta:
+        verbose_name = 'Aktor'
+        verbose_name_plural = 'Aktorzy'
 
 class Cast(models.Model):
-    character = models.CharField(max_length=60)
+    character = models.CharField(max_length=60, verbose_name='Postać')
 
     actor = ForeignKey('Actor', related_name='character',
-                       on_delete=CASCADE, null=True, blank=True)
+                       on_delete=CASCADE, null=True, blank=True, verbose_name='Aktor')
     movie = ForeignKey('Movie', related_name='character',
-                       on_delete=CASCADE, null=True, blank=True)
+                       on_delete=CASCADE, null=True, blank=True, verbose_name='Film')
 
     def __str__(self):
         return f"{self.actor} jako {self.character} w {self.movie}"
 
+    class Meta:
+        verbose_name = 'Obsada'
+        verbose_name_plural = 'Obsady'
+
 
 class UserProfile(models.Model):
     avatar = models.ImageField(
-        upload_to='avatars/', default='avatars/default.jpg')
+        upload_to='avatars/', default='avatars/default.jpg', verbose_name='Profilowe')
 
     user = models.ForeignKey(
-        User, related_name='profile', on_delete=CASCADE, null=True)
+        User, related_name='profile', on_delete=CASCADE, null=True, verbose_name='Użytkownik')
 
     def save(self, *args, **kwargs):
         super(UserProfile, self).save(*args, **kwargs)
@@ -148,3 +174,7 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return f"{self.user}"
+
+    class Meta:
+        verbose_name = 'Profil'
+        verbose_name_plural = 'Profile'
